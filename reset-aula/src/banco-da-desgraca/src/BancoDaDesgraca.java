8926b175939867a136826ddc9179b2a6e5d89837
JoseaneGrigolo <joseaneig@gmail.com>
2021-02-24 09:43:16 -0300

    finalizado

--- reset-aula/src/banco-da-desgraca/src/BancoDaDesgraca.java
+++ reset-aula/src/banco-da-desgraca/src/BancoDaDesgraca.java
@@ -14,10 +14,12 @@ public class BancoDaDesgraca {
 
         ContaBancaria conta1 = new ContaPoupanca(123, InstituicaoBancaria.CAIXA, 100.00);
         ContaBancaria conta2 = new ContaCorrente(456, InstituicaoBancaria.BRADESCO, 50.00);
-        ContaBancaria conta3 = new ContaDigital(789, InstituicaoBancaria.ITAU, 10.00);
+        ContaBancaria conta3 = new ContaDigital(789, InstituicaoBancaria.ITAU, 1000.00);
 
         conta1.sacar(50.00);
         conta1.transferir(30.00, conta2);
+
+        //Obs: Para ficar registrado, precisei de ajuda neste metodo!
         conta1.exibirExtrato(LocalDate.of(2020, 7, 5), LocalDate.of(2021, 3, 2));
     }
 }
